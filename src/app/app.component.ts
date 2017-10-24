d85bcb3ba28231108956924e7865cc4942e93885
Arjun Pawar <arjun.pawar@netapp.com>
2017-10-23 21:06:23 -0700

    clean code for column display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,55 +22,48 @@ function s2ab(s: string): ArrayBuffer {
 })
 export class AppComponent {
   private destiationFileValue: any;
+  private TableForFile1: any;
+  private TableForFile2: any;
 
   data: AOA;
-
   wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'binary' };
   fileName = 'Result.xlsx';
-
-  onFileChange(evt: any, table: AOA) {
-
-    console.log(table);
-    console.log(typeof(table));
-
-    /*  wire up file reader */
-
+onFileChange(evt: any, number) {
     const target: DataTransfer = <DataTransfer>(evt.target);
-	  if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    if (target.files.length !== 1) {
+      throw new Error('Cannot use multiple files');
+    }
     const reader: FileReader = new FileReader();
     reader.onload = (e: any) => {
-			/* read workbook */
-    const bstr: string = e.target.result;
-    const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-
-    /* grab first sheet */
-    const wsname: string = wb.SheetNames[0];
-			const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-			/* save data */
-      table = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
 
       // just return the column Headings
+      if (number === '1') {
+        this.TableForFile1 = this.data[0];
+      }
+      if (number === '2') {
+        this.TableForFile2 = this.data[0];
+      }
+    };
+    reader.readAsBinaryString(target.files[0]);
+}
 
-      table = table[0];
-      console.log('data');
-      console.log(table);
-		};
-		reader.readAsBinaryString(target.files[0]);
-	}
+export(): void {
+  /* generate worksheet */
+  const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
 
-	export(): void {
-		/* generate worksheet */
-		const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+  /* generate workbook and add the worksheet */
+  const wb: XLSX.WorkBook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
-		/* generate workbook and add the worksheet */
-		const wb: XLSX.WorkBook = XLSX.utils.book_new();
-		XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-		/* save to file */
-		const wbout: string = XLSX.write(wb, this.wopts);
-		saveAs(new Blob([s2ab(wbout)]), this.fileName);
-	}
+  /* save to file */
+  const wbout: string = XLSX.write(wb, this.wopts);
+  saveAs(new Blob([s2ab(wbout)]), this.fileName);
+}
 
   destinationFileFunction(val) {
     this.destiationFileValue = val;
